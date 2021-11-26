9dcb2a5d17f120d9e278972d5ce360afb5a3cb5d
Ade Yusuf <adeyusuf005@gmail.com>
2021-11-25 17:05:38 -0800

    Membuat template web e-commerce

---
+++ script.js
@@ -0,0 +1,43 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+    setTimeout(() => {
+        nav.classList.toggle('sticky', scrollY > 500);
+    }, 500);
+});
+
+const cart      = document.querySelectorAll('.cart-card');
+const harga     = document.querySelectorAll('.cart-card .cart-card-total input');
+const quantity  = document.querySelectorAll('.cart-card input');
+const span      = document.querySelectorAll('.cart-card-total h6 span');
+
+
+quantity.forEach(jumlah => {
+    jumlah.addEventListener('change', function() {
+        harga.forEach(h => {
+            if(this.id === h.id) {
+               const total = jumlah.value * h.value;
+               span.forEach(s => {
+                if(s.id == this.id) {
+                    s.innerHTML = total.toLocaleString('iD', 3);
+                }
+               });
+            }
+        });
+    });
+});
+
+
+const images    = document.querySelector('.product-image');
+const image     = document.querySelectorAll('.cards-images .card-image');
+const jumbo     = document.querySelector('.jumbo');
+
+images.addEventListener('click', function(e) {
+    if(e.target.className == 'p-img') {
+        jumbo.src = e.target.src;
+        setTimeout(() => {
+            jumbo.classList.remove('fade');
+        }, 500);
+        jumbo.classList.add('fade');
+    }
+});
